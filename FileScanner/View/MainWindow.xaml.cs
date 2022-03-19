2abbd264e0b9921177447612cc52e7479d8f288c
Somashekhar Muniyappa <somashekhar.muniyappa@hpe.com>
2022-03-19 22:20:02 +0530

    Working solution
    Completed file scanner to scan for text file

---
+++ FileScanner/View/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using FileScanner.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FileScanner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+        private void OnScrollChange(object sender, ScrollChangedEventArgs e)
+        {
+            ScrollViewer sb = e.OriginalSource as ScrollViewer;
+            if(sb.VerticalOffset == 0)
+            {
+                return;
+            }
+
+            if (sb.VerticalOffset == sb.ScrollableHeight)
+            {
+                var vm = ((MainViewModel)DataContext);
+                var count = vm.Customers.Count();
+                _ = vm.LoadMoreData();
+                sb.ScrollToVerticalOffset(count - 100);                                
+            }
+        }
+    }
+}
---
+++ FileScanner/ViewModel/MainViewModel.cs
@@ -0,0 +1,125 @@
+﻿using FileScanner.Commands;
+using FileScanner.Model;
+using Ookii.Dialogs.Wpf;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FileScanner.ViewModel
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<Customer> _customers;
+        private List<Customer> _customerList;
+        private List<FileInfo> fileInfos = new List<FileInfo>();
+        private int currentCount = 0;
+        private ICommand _clickCommand;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ICommand ClickCommand
+        {
+            get
+            {
+                return _clickCommand ?? (_clickCommand = new CommandHandler(() => SelectFolder(), () => CanExecute));
+            }
+        }
+        public bool CanExecute
+        {
+            get
+            {
+                // check if executing is allowed, i.e., validate, check if a process is running, etc. 
+                return true;
+            }
+        }
+
+        public ObservableCollection<Customer> Customers 
+        { 
+            get
+            {                
+                {
+                    return _customers;
+                }
+            }
+            private set
+            {
+                _customers = value;                
+                OnPropertyChanged("Customers");
+                
+            }
+                
+        }
+
+        public MainViewModel()
+        {
+            _customers = new ObservableCollection<Customer>();
+            _customerList = new List<Customer>();
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public async Task SelectFolder()
+        {
+            var dialog = new VistaFolderBrowserDialog();
+            dialog.Description = "Please select a folder.";
+            dialog.UseDescriptionForTitle = true;   
+            if ((bool)dialog.ShowDialog())
+            {
+                var path = dialog.SelectedPath;
+                var dirInfo = new DirectoryInfo(path);
+                fileInfos = dirInfo.GetFiles("*.txt", SearchOption.AllDirectories).ToList();
+                await LoadMoreData();
+            }
+        }
+
+        public async Task LoadMoreData()
+        {            
+            var listToprocess = fileInfos.Skip(currentCount).Take(100);
+            currentCount += listToprocess.Count();
+
+            foreach (var file in listToprocess)
+            {
+                var customersFound = await GetFileText(file.FullName);
+                if (customersFound.Count() > 0)
+                {
+                    _customerList.AddRange(customersFound);
+                    Customers = new ObservableCollection<Customer>(_customerList);
+                }
+            }
+        }
+
+        private async Task<List<Customer>> GetFileText(string filePath)
+        {
+            var fileContents = new List<string>();
+            string customerInfo = @"CustomerInfo:";
+            string nameString = @"Name:";
+            string phoneString = @"PhoneNumber:";
+            string addressString = @"Address:";
+            var customers = new List<Customer>();
+            
+            if (File.Exists(filePath))
+            {
+                var content = await File.ReadAllLinesAsync(filePath);
+                fileContents = content.ToList();
+                var customerValue = fileContents.Where(x => x.StartsWith(customerInfo)).Select(x => x.Remove(0, customerInfo.Length));   
+                foreach(var customerData in customerValue) 
+                {
+                    var values = customerData.Split(';').ToList();
+                    var nameValue = values.Where(x => x.StartsWith(nameString)).Select(x => x.Remove(0, nameString.Length)).ToList();
+                    var addressValue = values.Where(x => x.StartsWith(addressString)).Select(x => x.Remove(0, addressString.Length)).ToList();
+                    var phoneValue = values.Where(x => x.StartsWith(phoneString)).Select(x => x.Remove(0, phoneString.Length)).ToList();
+                    var customer = new Customer { Id = this.Customers.Count() + customers.Count() + 1, Name = nameValue[0], Address = addressValue[0], PhoneNumber = phoneValue[0] };
+                    customers.Add(customer);
+                }
+            }
+            return customers;
+        }
+    }
+}
